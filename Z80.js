19420f837b3382f06a274d01434b959a3dfa34c9
rteast520 <rteast520@gmail.com>
2025-04-12 10:38:04 -0400

    Register Object Created

---
+++ Z80.js
@@ -0,0 +1,12 @@
+
+Z80 = {
+	//time clock m and t
+	_clock: {m:0, t:0},
+
+	//registers of the Z80
+	_r: {
+		a:0, b:0, c:0, d:0, e:0, h:0, l:0, f:0, /*F is a flag register producres Zero, Operation, Halfcarry and carry*/
+		pc:0, sp:0,
+		m:0, t:0
+	}
+}
