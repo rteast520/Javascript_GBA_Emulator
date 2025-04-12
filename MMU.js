71c775ad804b96d3655721a9bb47d078a6c7dd45
rteast520 <rteast520@gmail.com>
2025-04-12 11:25:41 -0400

    fixed error in write function: left out val to be written

--- MMU.js
+++ MMU.js
@@ -3,6 +3,6 @@ MMU = {
     rb: function(addr){/*read 8-bitd from addr*/},
     rw: function(addr){},//write to 16 bit addr,
 
-    wb: function(addr){},//write to 8 bit addr
-    ww: function(addr){}, //write 16bit to addr
+    wb: function(addr, val){},//write to 8 bit addr
+    ww: function(addr, val){}, //write 16bit to addr
 };
