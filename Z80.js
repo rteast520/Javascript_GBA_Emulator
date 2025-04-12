e93dac4ec7863e92658d6774e46ac54b42836235
rteast520 <rteast520@gmail.com>
2025-04-12 11:09:39 -0400

    Created addr function

--- Z80.js
+++ Z80.js
@@ -8,5 +8,17 @@ Z80 = {
 		a:0, b:0, c:0, d:0, e:0, h:0, l:0, f:0, /*F is a flag register producres Zero, Operation, Halfcarry and carry*/
 		pc:0, sp:0,
 		m:0, t:0
+	},
+
+	//adds e to a nad leaves result in reg a
+	ADDr_e: function(){
+		Z80._r.a += Z80._r.e; //add
+		z80._r.f = 0; //clear flag
+		if(!(Z80._r.a & 255)) Z80._r.f |=0x80; //check for zero
+		if(Z80._r.a > 255) Z80._r.f |= 0x10; //check for a carry
+		Z80._r.a &= 255; //mask bits into a
+		//update time regs
+		Z80._r.m = 1;
+		Z80._r.t = 4; 
 	}
 }
